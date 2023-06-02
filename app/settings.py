ec6956ed4014ccd6abf82615f96fb250cd5d01c6
Ivan Lapchenko <lapchenko.vanya@gmail.com>
2023-06-02 20:03:51 +0300

    work with authorization

--- app/settings.py
+++ app/settings.py
@@ -38,7 +38,9 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'blog',
+    'authorization',
     'reset_migrations',
+    'widget_tweaks',
 ]
 
 MIDDLEWARE = [
---
+++ authorization/views.py
@@ -0,0 +1,22 @@
+from django.urls import reverse_lazy
+from django.views.generic import CreateView
+from .forms import SignupForm
+from django.contrib import messages
+
+
+class SignupView(CreateView):
+    form_class = SignupForm
+    template_name = 'registration/signup.html'
+    success_url = reverse_lazy('login')
+
+    def form_invalid(self, form):
+        for fields, errors in form.errors.items():
+            for error in errors:
+                messages.warning(self.request, error)
+        return super().form_invalid(form)
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['title'] = 'Register'
+        return context
+
